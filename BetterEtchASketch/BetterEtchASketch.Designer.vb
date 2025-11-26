8ae907b8416e55020f6b4b399ebcd3f00abc4cc7
Noah Holloway <noahholloway23@gmail.com>
2025-11-26 16:45:32 -0700

    Add project files.

---
+++ BetterEtchASketch/BetterEtchASketch.Designer.vb
@@ -0,0 +1,239 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class BetterEtchASketch
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DisplayPictureBox = New System.Windows.Forms.PictureBox()
+        Me.MouseModeRadioButton = New System.Windows.Forms.RadioButton()
+        Me.ExternalModeRadioButton = New System.Windows.Forms.RadioButton()
+        Me.SelectColorButton = New System.Windows.Forms.Button()
+        Me.DrawWaveformsButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.MainMenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.FileMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SelectColorMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DrawWaveformsMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ControlToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.PortComboBox = New System.Windows.Forms.ComboBox()
+        Me.ConnectButton = New System.Windows.Forms.Button()
+        CType(Me.DisplayPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MainMenuStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DisplayPictureBox
+        '
+        Me.DisplayPictureBox.Location = New System.Drawing.Point(12, 33)
+        Me.DisplayPictureBox.Name = "DisplayPictureBox"
+        Me.DisplayPictureBox.Size = New System.Drawing.Size(661, 405)
+        Me.DisplayPictureBox.TabIndex = 0
+        Me.DisplayPictureBox.TabStop = False
+        '
+        'MouseModeRadioButton
+        '
+        Me.MouseModeRadioButton.AutoSize = True
+        Me.MouseModeRadioButton.Checked = True
+        Me.MouseModeRadioButton.Location = New System.Drawing.Point(679, 207)
+        Me.MouseModeRadioButton.Name = "MouseModeRadioButton"
+        Me.MouseModeRadioButton.Size = New System.Drawing.Size(107, 20)
+        Me.MouseModeRadioButton.TabIndex = 1
+        Me.MouseModeRadioButton.TabStop = True
+        Me.MouseModeRadioButton.Text = "Mouse Mode"
+        Me.MouseModeRadioButton.UseVisualStyleBackColor = True
+        '
+        'ExternalModeRadioButton
+        '
+        Me.ExternalModeRadioButton.AutoSize = True
+        Me.ExternalModeRadioButton.Location = New System.Drawing.Point(679, 233)
+        Me.ExternalModeRadioButton.Name = "ExternalModeRadioButton"
+        Me.ExternalModeRadioButton.Size = New System.Drawing.Size(90, 20)
+        Me.ExternalModeRadioButton.TabIndex = 2
+        Me.ExternalModeRadioButton.Text = "Q@ Mode"
+        Me.ExternalModeRadioButton.UseVisualStyleBackColor = True
+        '
+        'SelectColorButton
+        '
+        Me.SelectColorButton.Location = New System.Drawing.Point(684, 276)
+        Me.SelectColorButton.Name = "SelectColorButton"
+        Me.SelectColorButton.Size = New System.Drawing.Size(85, 35)
+        Me.SelectColorButton.TabIndex = 3
+        Me.SelectColorButton.Text = "&Color"
+        Me.SelectColorButton.UseVisualStyleBackColor = True
+        '
+        'DrawWaveformsButton
+        '
+        Me.DrawWaveformsButton.Location = New System.Drawing.Point(684, 317)
+        Me.DrawWaveformsButton.Name = "DrawWaveformsButton"
+        Me.DrawWaveformsButton.Size = New System.Drawing.Size(85, 35)
+        Me.DrawWaveformsButton.TabIndex = 4
+        Me.DrawWaveformsButton.Text = "&Waveforms"
+        Me.DrawWaveformsButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(684, 358)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(85, 35)
+        Me.ClearButton.TabIndex = 5
+        Me.ClearButton.Text = "&Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(684, 403)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(85, 35)
+        Me.ExitButton.TabIndex = 6
+        Me.ExitButton.Text = "&Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'MainMenuStrip
+        '
+        Me.MainMenuStrip.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MainMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileMenuItem, Me.EditMenuItem, Me.HelpMenuItem})
+        Me.MainMenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.MainMenuStrip.Name = "MainMenuStrip"
+        Me.MainMenuStrip.Size = New System.Drawing.Size(800, 28)
+        Me.MainMenuStrip.TabIndex = 7
+        Me.MainMenuStrip.Text = "MenuStrip1"
+        '
+        'FileMenuItem
+        '
+        Me.FileMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitMenuItem})
+        Me.FileMenuItem.Name = "FileMenuItem"
+        Me.FileMenuItem.Size = New System.Drawing.Size(46, 24)
+        Me.FileMenuItem.Text = "File"
+        '
+        'ExitMenuItem
+        '
+        Me.ExitMenuItem.Name = "ExitMenuItem"
+        Me.ExitMenuItem.Size = New System.Drawing.Size(116, 26)
+        Me.ExitMenuItem.Text = "Exit"
+        '
+        'EditMenuItem
+        '
+        Me.EditMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SelectColorMenuItem, Me.DrawWaveformsMenuItem, Me.ClearMenuItem})
+        Me.EditMenuItem.Name = "EditMenuItem"
+        Me.EditMenuItem.Size = New System.Drawing.Size(49, 24)
+        Me.EditMenuItem.Text = "Edit"
+        '
+        'SelectColorMenuItem
+        '
+        Me.SelectColorMenuItem.Name = "SelectColorMenuItem"
+        Me.SelectColorMenuItem.Size = New System.Drawing.Size(205, 26)
+        Me.SelectColorMenuItem.Text = "Select Color"
+        '
+        'DrawWaveformsMenuItem
+        '
+        Me.DrawWaveformsMenuItem.Name = "DrawWaveformsMenuItem"
+        Me.DrawWaveformsMenuItem.Size = New System.Drawing.Size(205, 26)
+        Me.DrawWaveformsMenuItem.Text = "Draw Waveforms"
+        '
+        'ClearMenuItem
+        '
+        Me.ClearMenuItem.Name = "ClearMenuItem"
+        Me.ClearMenuItem.Size = New System.Drawing.Size(205, 26)
+        Me.ClearMenuItem.Text = "Clear"
+        '
+        'HelpMenuItem
+        '
+        Me.HelpMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutMenuItem})
+        Me.HelpMenuItem.Name = "HelpMenuItem"
+        Me.HelpMenuItem.Size = New System.Drawing.Size(55, 24)
+        Me.HelpMenuItem.Text = "Help"
+        '
+        'AboutMenuItem
+        '
+        Me.AboutMenuItem.Name = "AboutMenuItem"
+        Me.AboutMenuItem.Size = New System.Drawing.Size(133, 26)
+        Me.AboutMenuItem.Text = "About"
+        '
+        'PortComboBox
+        '
+        Me.PortComboBox.FormattingEnabled = True
+        Me.PortComboBox.Location = New System.Drawing.Point(684, 87)
+        Me.PortComboBox.Name = "PortComboBox"
+        Me.PortComboBox.Size = New System.Drawing.Size(102, 24)
+        Me.PortComboBox.TabIndex = 8
+        '
+        'ConnectButton
+        '
+        Me.ConnectButton.Location = New System.Drawing.Point(684, 128)
+        Me.ConnectButton.Name = "ConnectButton"
+        Me.ConnectButton.Size = New System.Drawing.Size(85, 35)
+        Me.ConnectButton.TabIndex = 9
+        Me.ConnectButton.Text = "Connect"
+        Me.ConnectButton.UseVisualStyleBackColor = True
+        '
+        'BetterEtchASketch
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ConnectButton)
+        Me.Controls.Add(Me.PortComboBox)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.DrawWaveformsButton)
+        Me.Controls.Add(Me.SelectColorButton)
+        Me.Controls.Add(Me.ExternalModeRadioButton)
+        Me.Controls.Add(Me.MouseModeRadioButton)
+        Me.Controls.Add(Me.DisplayPictureBox)
+        Me.Controls.Add(Me.MainMenuStrip)
+        Me.Name = "BetterEtchASketch"
+        Me.Text = "Etch-A-Sketch"
+        CType(Me.DisplayPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MainMenuStrip.ResumeLayout(False)
+        Me.MainMenuStrip.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DisplayPictureBox As PictureBox
+    Friend WithEvents MouseModeRadioButton As RadioButton
+    Friend WithEvents ExternalModeRadioButton As RadioButton
+    Friend WithEvents SelectColorButton As Button
+    Friend WithEvents DrawWaveformsButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents MainMenuStrip As MenuStrip
+    Friend WithEvents FileMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitMenuItem As ToolStripMenuItem
+    Friend WithEvents EditMenuItem As ToolStripMenuItem
+    Friend WithEvents SelectColorMenuItem As ToolStripMenuItem
+    Friend WithEvents DrawWaveformsMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutMenuItem As ToolStripMenuItem
+    Friend WithEvents ControlToolTip As ToolTip
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents PortComboBox As ComboBox
+    Friend WithEvents ConnectButton As Button
+End Class
---
+++ BetterEtchASketch/BetterEtchASketch.vb
@@ -0,0 +1,250 @@
+﻿'Noah Holloway
+'RCET 3371
+'Fall 2025
+'Better Etch-A-Sketch
+
+Imports System.IO.Ports
+Imports System.Drawing.Drawing2D
+Imports System.Media
+
+Public Class BetterEtchASketch
+    ' Current drawing color
+    Private currentColor As Color = Color.Black
+    ' Graphics object for drawing
+    Private g As Graphics
+    ' For drawing lines
+    Private penPosition As Point
+    ' Serial port for Quiet Board
+    Private WithEvents PICSerialPort As New SerialPort()
+    ' Timers for sending requests and reading data
+    Private WithEvents CommandTimer As New Timer()
+    Private WithEvents ReadTimer As New Timer()
+    ' Mouse drawing flag
+    Private isDrawing As Boolean = False
+
+    ' ---------------- Form Load ----------------
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        g = DisplayPictureBox.CreateGraphics()
+        DisplayPictureBox.BackColor = Color.White
+
+        PopulatePorts()
+
+        ' Initialize timers
+        CommandTimer.Interval = 50   ' request analog every 50ms
+        ReadTimer.Interval = 10      ' read buffer every 10ms
+
+        ' Tooltips
+        ControlToolTip.SetToolTip(DisplayPictureBox, "Drawing area")
+        ControlToolTip.SetToolTip(MouseModeRadioButton, "Draw with mouse")
+        ControlToolTip.SetToolTip(ExternalModeRadioButton, "Draw with PIC potentiometers")
+        ControlToolTip.SetToolTip(SelectColorButton, "Select drawing color")
+        ControlToolTip.SetToolTip(DrawWaveformsButton, "Draw waveforms")
+        ControlToolTip.SetToolTip(ClearButton, "Clear display")
+        ControlToolTip.SetToolTip(ExitButton, "Exit program")
+    End Sub
+
+    ' ---------------- Populate COM Ports ----------------
+    Private Sub PopulatePorts()
+        PortComboBox.Items.Clear()
+        PortComboBox.Items.AddRange(SerialPort.GetPortNames())
+        If PortComboBox.Items.Count > 0 Then PortComboBox.SelectedIndex = 0
+    End Sub
+
+    Private Sub PortComboBox_DropDown(sender As Object, e As EventArgs) Handles PortComboBox.DropDown
+        PopulatePorts()
+    End Sub
+
+    ' ---------------- Connect to Quiet Board ----------------
+    Private Sub ConnectButton_Click(sender As Object, e As EventArgs) Handles ConnectButton.Click
+        If PortComboBox.SelectedItem Is Nothing Then
+            MessageBox.Show("Select a COM port first")
+            Return
+        End If
+
+        Try
+            If PICSerialPort.IsOpen Then PICSerialPort.Close()
+            PICSerialPort.PortName = PortComboBox.SelectedItem.ToString()
+            PICSerialPort.BaudRate = 9600
+            PICSerialPort.DataBits = 8
+            PICSerialPort.Parity = Parity.None
+            PICSerialPort.StopBits = StopBits.One
+            PICSerialPort.Open()
+            MessageBox.Show($"Connected to {PICSerialPort.PortName}")
+
+            If ExternalModeRadioButton.Checked Then
+                CommandTimer.Start()
+                ReadTimer.Start()
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show($"Failed to open port: {ex.Message}")
+        End Try
+    End Sub
+
+    ' ---------------- Mouse Drawing ----------------
+    Private Sub DisplayPictureBox_MouseDown(sender As Object, e As MouseEventArgs) Handles DisplayPictureBox.MouseDown
+        If MouseModeRadioButton.Checked AndAlso e.Button = MouseButtons.Left Then
+            isDrawing = True
+            penPosition = e.Location
+        ElseIf e.Button = MouseButtons.Middle Then
+            SelectColor()
+        End If
+    End Sub
+
+    Private Sub DisplayPictureBox_MouseMove(sender As Object, e As MouseEventArgs) Handles DisplayPictureBox.MouseMove
+        If isDrawing AndAlso MouseModeRadioButton.Checked Then
+            g.DrawLine(New Pen(currentColor), penPosition, e.Location)
+            penPosition = e.Location
+        End If
+    End Sub
+
+    Private Sub DisplayPictureBox_MouseUp(sender As Object, e As MouseEventArgs) Handles DisplayPictureBox.MouseUp
+        isDrawing = False
+    End Sub
+
+    ' ---------------- External Mode Drawing ----------------
+    Private Sub ExternalModeRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles ExternalModeRadioButton.CheckedChanged
+        If ExternalModeRadioButton.Checked Then
+            penPosition = New Point(DisplayPictureBox.Width \ 2, DisplayPictureBox.Height \ 2)
+            If PICSerialPort.IsOpen Then
+                CommandTimer.Start()
+                ReadTimer.Start()
+            End If
+        Else
+            CommandTimer.Stop()
+            ReadTimer.Stop()
+        End If
+    End Sub
+
+    ' ---------------- Command Timer ----------------
+    Private Sub CommandTimer_Tick(sender As Object, e As EventArgs) Handles CommandTimer.Tick
+        If PICSerialPort.IsOpen AndAlso ExternalModeRadioButton.Checked Then
+            Const RequestAnalogCommand As Byte = &H53
+            PICSerialPort.Write(New Byte() {RequestAnalogCommand}, 0, 1)
+        End If
+    End Sub
+
+    ' ---------------- Read Timer ----------------
+    Private Sub ReadTimer_Tick(sender As Object, e As EventArgs) Handles ReadTimer.Tick
+        If Not PICSerialPort.IsOpen Then Exit Sub
+        While PICSerialPort.BytesToRead >= 4
+            Dim b(3) As Byte
+            Dim read As Integer = PICSerialPort.Read(b, 0, 4)
+            If read < 4 Then Exit While
+            ProcessQuietBoardPacket(b)
+        End While
+    End Sub
+
+    ' ---------------- Process 4-byte Quiet Board packet ----------------
+    Private Sub ProcessQuietBoardPacket(b() As Byte)
+        Dim x As Integer = (CInt(b(0)) << 2) Or (b(1) >> 6)
+        Dim y As Integer = (CInt(b(2)) << 2) Or (b(3) >> 6)
+
+        Dim mappedX As Integer = CInt(x * DisplayPictureBox.Width / 1023)
+        Dim mappedY As Integer = CInt(y * DisplayPictureBox.Height / 1023)
+        Dim newPoint As New Point(mappedX, mappedY)
+
+        If ExternalModeRadioButton.Checked Then
+            Me.Invoke(Sub()
+                          g.DrawLine(New Pen(currentColor), penPosition, newPoint)
+                          penPosition = newPoint
+                      End Sub)
+        Else
+            penPosition = newPoint
+        End If
+    End Sub
+
+    ' ---------------- Color Selection ----------------
+    Private Sub SelectColorButton_Click(sender As Object, e As EventArgs) Handles SelectColorButton.Click
+        SelectColor()
+    End Sub
+
+    Private Sub SelectColorMenuItem_Click(sender As Object, e As EventArgs) Handles SelectColorMenuItem.Click
+        SelectColor()
+    End Sub
+
+    Private Sub SelectColor()
+        Using dlg As New ColorDialog()
+            If dlg.ShowDialog() = DialogResult.OK Then
+                currentColor = dlg.Color
+            End If
+        End Using
+    End Sub
+
+    ' ---------------- Clear Display ----------------
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        ShakeDisplay()
+        g.Clear(Color.White)
+    End Sub
+
+    Private Sub ClearMenuItem_Click(sender As Object, e As EventArgs) Handles ClearMenuItem.Click
+        ClearButton.PerformClick()
+    End Sub
+
+    Private Sub ShakeDisplay()
+        Dim originalLocation As Point = DisplayPictureBox.Location
+        For i As Integer = 1 To 10
+            DisplayPictureBox.Location = New Point(originalLocation.X + If(i Mod 2 = 0, 5, -5), originalLocation.Y)
+            Me.Refresh()
+            Threading.Thread.Sleep(20)
+        Next
+        DisplayPictureBox.Location = originalLocation
+    End Sub
+
+    ' ---------------- Draw Waveforms ----------------
+    Private Sub DrawWaveformsButton_Click(sender As Object, e As EventArgs) Handles DrawWaveformsButton.Click
+        g.Clear(Color.White)
+        DrawGraticule()
+        DrawSineCosTan()
+    End Sub
+
+    Private Sub DrawWaveformsMenuItem_Click(sender As Object, e As EventArgs) Handles DrawWaveformsMenuItem.Click
+        DrawWaveformsButton.PerformClick()
+    End Sub
+
+    Private Sub DrawGraticule()
+        Dim pen As New Pen(Color.LightGray)
+        Dim cellWidth As Integer = DisplayPictureBox.Width \ 10
+        Dim cellHeight As Integer = DisplayPictureBox.Height \ 10
+
+        For i As Integer = 0 To 10
+            g.DrawLine(pen, i * cellWidth, 0, i * cellWidth, DisplayPictureBox.Height)
+            g.DrawLine(pen, 0, i * cellHeight, DisplayPictureBox.Width, i * cellHeight)
+        Next
+    End Sub
+
+    Private Sub DrawSineCosTan()
+        Dim w As Integer = DisplayPictureBox.Width
+        Dim h As Integer = DisplayPictureBox.Height
+        Dim penSine As New Pen(Color.Red)
+        Dim penCos As New Pen(Color.Blue)
+        Dim penTan As New Pen(Color.Green)
+
+        For i As Integer = 0 To w - 2
+            Dim xVal As Double = 2 * Math.PI * i / w
+            Dim ySine As Integer = CInt(h / 2 - (h / 4) * Math.Sin(xVal))
+            Dim yCos As Integer = CInt(h / 2 - (h / 4) * Math.Cos(xVal))
+            Dim yTan As Integer = CInt(h / 2 - (h / 8) * Math.Tan(xVal))
+            yTan = Math.Max(0, Math.Min(h - 1, yTan))
+            g.DrawLine(penSine, i, ySine, i + 1, CInt(h / 2 - (h / 4) * Math.Sin(2 * Math.PI * (i + 1) / w)))
+            g.DrawLine(penCos, i, yCos, i + 1, CInt(h / 2 - (h / 4) * Math.Cos(2 * Math.PI * (i + 1) / w)))
+            g.DrawLine(penTan, i, yTan, i + 1, CInt(h / 2 - (h / 8) * Math.Tan(2 * Math.PI * (i + 1) / w)))
+        Next
+    End Sub
+
+    ' ---------------- Exit ----------------
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ExitMenuItem_Click(sender As Object, e As EventArgs) Handles ExitMenuItem.Click
+        ExitButton.PerformClick()
+    End Sub
+
+    ' ---------------- About ----------------
+    Private Sub AboutMenuItem_Click(sender As Object, e As EventArgs) Handles AboutMenuItem.Click
+        MessageBox.Show("Better Etch-A-Sketch v1.0" & vbCrLf & "Created for PIC Assignment", "About")
+    End Sub
+End Class
+
+
